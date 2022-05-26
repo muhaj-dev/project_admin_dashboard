3d2cc2909bf8e925d9f34c78011bbc79401e29ae
muhammod1 <muhammodholawale@gmail.com>
2022-05-26 23:18:44 +0100

    refactor

---
+++ src/components/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>Chat</div>
+  )
+}
+
+export default Cart;
---
+++ src/pages/Kanban.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Kanban = () => {
+  return (
+    <div>kanbar</div>
+  )
+}
+
+export default Kanban
